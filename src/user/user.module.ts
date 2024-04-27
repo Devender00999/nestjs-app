846eb41b99bbfc64176a7a14524f834e21c647a0
Devender Kumar <devender@rubick.ai>
2024-04-27 11:27:24 +0530

    Added a middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { LoggerMiddleware } from './middleware';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
